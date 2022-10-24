094f52a176c4e4fa94db397efe17636ea8e506fb
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2022-10-24 07:22:21 +0200

    Revert "Configmap actyoins"
    This reverts commit a25fe2db558616781b8eb4409164d94b032252b3.

--- internal/testing-samples/multitestCaller/main_test.go
+++ internal/testing-samples/multitestCaller/main_test.go
@@ -1,13 +1,11 @@
-package multitestCaller
+package multitestcaller
 
 import (
 	"context"
 	"fmt"
 	"testing"
 
-	"github.com/aws/aws-sdk-go/service/ec2"
 	hyperv1 "github.com/openshift/hypershift/api/v1alpha1"
-	awsutil "github.com/openshift/hypershift/cmd/infra/aws/util"
 	e2eutil "github.com/openshift/hypershift/test/e2e/util"
 
 	//corev1 "k8s.io/api/core/v1"
@@ -23,37 +21,27 @@ const (
 )
 
 func TestNodePoolMain(t *testing.T) {
+	t.Parallel()
 	g := NewWithT(t)
 
 	ctx, cancel := context.WithCancel(context.TODO())
-	clusterOpts := globalOpts.DefaultClusterOptions(t)
 	defer cancel()
 
-	mgmtClient, err := e2eutil.GetClient()
+	client, err := e2eutil.GetClient()
 	g.Expect(err).NotTo(HaveOccurred(), "failed to get k8s client")
 
 	guestClusters := &hyperv1.HostedClusterList{}
-	err = mgmtClient.List(ctx, guestClusters, &crclient.ListOptions{
+	err = client.List(ctx, guestClusters, &crclient.ListOptions{
 		Namespace: ns,
 	})
 	g.Expect(err).NotTo(HaveOccurred(), "failed to get k8s already created Cluster")
 
-	guestClient := e2eutil.WaitForGuestClient(t, ctx, mgmtClient, &guestClusters.Items[0])
+	//guestClient := e2eutil.WaitForGuestClient(&t, ctx, client, &guestClusters.Items[0])
 	guestCluster := &guestClusters.Items[0]
 
 	fmt.Println(guestCluster)
 	g.Expect(guestCluster.Name).To(Equal(hcName))
 
-	// Wait for the rollout to be reported complete
-	t.Logf("Waiting for cluster rollout. Image: %s", globalOpts.LatestReleaseImage)
-	e2eutil.WaitForImageRollout(t, ctx, mgmtClient, guestClient, guestCluster, globalOpts.LatestReleaseImage)
-
-	t.Run("TestAutoRepair", testAutoRepair(ctx, mgmtClient, guestCluster, guestClient, clusterOpts))
+	//t.Run("KillRandomMembers", testKillRandomMembers(ctx, client, cluster))
 	//t.Run("KillAllMembers", testKillAllMembers(ctx, client, cluster))
-}
-
-func ec2Client(awsCredsFile, region string) *ec2.EC2 {
-	awsSession := awsutil.NewSession("e2e-autorepair", awsCredsFile, "", "", region)
-	awsConfig := awsutil.NewConfig()
-	return ec2.New(awsSession, awsConfig)
 }
